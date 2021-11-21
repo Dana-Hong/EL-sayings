21382b7ace38ae3c661ad248b92bd97c50826d71
Dana <dana.hong@hotmail.com>
2021-11-21 15:18:13 -0500

    importing files from local

---
+++ script.js
@@ -0,0 +1,40 @@
+let btn = document.getElementById("btn-bg")
+let sayingP = document.getElementById("Saying")
+
+let sayingsList = [
+    `"it's yummy"`,
+    `"fun!"`,
+    `"interesting"`,
+    `"hmm"`,
+    `"mmhmm"`,
+    `"mmmm"`,
+    `"eeek"`,
+    `"ridonculous"`,
+    `"kewl"`,
+    `"noice"`,
+    `"cute"`,
+    `"silly"`,
+    `"VapoRub"`,
+    `"did you drink your cough syrup?"`,
+    `"I took a nap"`,
+    `"¿¿¿ ¿¿¿¿?"`
+
+]
+
+const removeSaying = () => {
+    let elem = document.getElementById("placeholderID");
+    elem.remove();
+}
+
+const generateSaying = () => {
+    let p = document.createElement("p");
+    p.id = "placeholderID"
+    let nmbrSayings = sayingsList.length;
+    let saying = sayingsList[Math.floor(Math.random() * nmbrSayings)]
+    p.appendChild(document.createTextNode(saying));
+    console.log(p)
+    sayingP.appendChild(p);
+}
+
+btn.addEventListener("click", removeSaying);
+btn.addEventListener("click", generateSaying);
